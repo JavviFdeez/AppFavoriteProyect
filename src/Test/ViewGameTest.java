bce6748a92056ac4c0cb3a9a145e49d918c98028
JavviFdeez <javifdez195@gmail.com>
2024-02-02 12:59:47 +0100

    Creación paqueteTest

---
+++ src/Test/ViewGameTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import View.ViewGame;
+import Interface.View.IViewGame;
+import org.junit.Test;
+
+public class ViewGameTest {
+
+    @Test
+    public void testDisplayMessageWelcome() {
+
+    }
+}
