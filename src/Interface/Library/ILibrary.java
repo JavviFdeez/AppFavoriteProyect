5adaac43548fcd873c5e9e83fde58ec083d7d776
Alberto <albertoromeropino2004@gmail.com>
2024-02-06 10:49:11 +0100

    primer_acto

--- src/Interface/Library/ILibrary.java
+++ src/Interface/Library/ILibrary.java
@@ -1,4 +1,26 @@
 package Interface.Library;
 
+import Model.Entity.Favorite;
+
 public interface ILibrary {
+    //Opciones obligatorias
+    public void crud(int opcion);
+
+    // 1 listar && 2 buscar:
+
+    public void findAll();
+    public void findByld();
+    public void findByClass();
+    public void findByName();
+
+    //Opciones que creo que son importantes para el crud
+
+    // 3 insertar
+    public void insertFavorite(int id, String name, String category);
+
+    // 4 actualizar
+
+    // 5 borrar
+    public void deleteFavorite(String name);
+
 }
--- src/Model/Library/Library.java
+++ src/Model/Library/Library.java
@@ -1,4 +1,56 @@
 package Model.Library;
 
-public class Library {
+import Interface.Library.ILibrary;
+import Model.Entity.Favorite;
+
+public class Library implements ILibrary {
+    private Favorite[] favorite;
+    @Override
+    public void crud(int opcion) {
+
+    }
+
+    @Override
+    public void findAll() {
+        for (int i = 0; i<favorite.length; i++){
+            System.out.println(favorite[i]);
+        }
+    }
+
+    @Override
+    public void findByld() {
+
+    }
+
+    @Override
+    public void findByClass() {
+
+    }
+
+    @Override
+    public void findByName() {
+        for (int i = 0; i<favorite.length; i++){
+            System.out.println(favorite[i].getname());
+        }
+    }
+
+    @Override
+    public void insertFavorite(int id, String name, String category) {
+
+
+    }
+
+    @Override
+    public void deleteFavorite(String name) {
+        for (int i = 0; i<favorite.length; i++){
+            if (favorite[i].getname() == name){
+                if (favorite[i].getname().equals(name)){
+                    favorite[i].setId(0);
+                    favorite[i].setname(null);
+                    favorite[i].setcategory(null);
+
+                }
+            }
+        }
+    }
 }
